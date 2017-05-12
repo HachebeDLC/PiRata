623c2a979af9f8ef3a4a9928c5c9884198571320
HachebeDLC <hbgam3r@hotmail.es>
2017-05-12 18:26:34 +0200

    Updated with Nginx as server
    Cambio en la instalacion a Nginx

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 sudo apt-get install apt-transport-https -y --force-yes;  
 wget -O - https://dev2day.de/pms/dev2day-pms.gpg.key  | sudo apt-key add -  ;
 echo "deb https://dev2day.de/pms/ jessie main" | sudo tee /etc/apt/sources.list.d/pms.list  ;
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
+echo "deb http://apt.sonarr.tv/ master main" | sudo tee /etc/apt/sources.list.d/sonarr.list
 sudo apt-get update  ;
-sudo apt-get install -t jessie plexmediaserver deluged deluge-console deluge-web apache2 -y  ;
+sudo apt-get install -t jessie plexmediaserver deluged deluge-console deluge-web nginx -y  ;
 echo pi:Hochobo:10 >> ~/.config/deluge/auth
-
-sudo cp -R  PiRata/apache2 /etc/apache2
-sudo cp -R www/ /var/
-sudo a2enmod proxy proxy_html proxy_http headers
-sudo service apache2 restart
 sudo git clone http://github.com/RuudBurger/CouchPotatoServer.git
+sudo apt-get update
+sudo apt-get install nzbdrone 
